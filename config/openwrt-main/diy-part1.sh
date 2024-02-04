6b53c0063fc5658e27f1560701f6882e0381c698
hetaozhen <hetaozhen@gmail.com>
2024-02-04 12:06:55 +0800

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -8,6 +8,12 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >> ./feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small.git' >> ./feeds.conf.default
+echo 'src-git dockerman https://github.com/lisaac/luci-app-dockerman.git' >> ./feeds.conf.default
+echo 'src-git diskman https://github.com/lisaac/luci-app-diskman.git' >> ./feeds.conf.default
+echo 'src-git netdata https://github.com/muink/luci-app-netdata.git' >> ./feeds.conf.default
+echo 'src-git netdata_ssl https://github.com/muink/openwrt-netdata-ssl.git' >> ./feeds.conf.default
 
 # other
 # rm -rf package/utils/{ucode,fbtest}
